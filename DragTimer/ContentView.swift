90abca19e9b9958174d39f48aab8c46c9246e85c
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-08-15 09:49:50 +0200

    Initial Commit

---
+++ DragTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DragTimer
+//
+//  Created by Jan Andrzejewski on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragTimer/DragTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragTimerApp.swift
+//  DragTimer
+//
+//  Created by Jan Andrzejewski on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DragTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
