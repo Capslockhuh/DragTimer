81ad0a992ee4a9e992708f7fb02c301d4c11c318
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-08-15 10:05:23 +0200

    Added dragging functionality and adding values

--- DragTimer/ContentView.swift
+++ DragTimer/ContentView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    @State private var timerValue = 0.0
+    let timer = Timer.publish(every: 1, on: .main, in: .common)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Set timer for \(Int(timerValue))")
+                .padding(5)
+                .font(.title)
+                .background(.blue)
+                .offset(dragAmount)
+                .animation(.default, value: dragAmount)
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation
+                    print(dragAmount)
+                    timerValue += 0.5
+                }
+                .onEnded { _ in
+                    dragAmount = .zero
+                }
+        )
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
